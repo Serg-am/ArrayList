3ec2b4bbdeaf410cf45a74e4971bebc7ae8fe0a0
Sergey <serg_am@live.ru>
2023-10-08 16:14:45 +0300

    Добавлена сортировка

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,14 @@
+import java.util.Comparator;
 
 public class Main {
     public static void main(String[] args) {
         MyArrayList<Integer> myArrayList = new MyArrayList<>();
-        myArrayList.add(1);
+        myArrayList.add(13);
         myArrayList.add(2);
         myArrayList.add(3);
+        myArrayList.add(8);
+        myArrayList.add(26);
+        myArrayList.add(11);
 
         System.out.println("Size: " + myArrayList.size());
         System.out.println("Elements: " + myArrayList);
@@ -12,6 +16,14 @@ public class Main {
 
         myArrayList.delete(1);
         System.out.println("After deleting index 1: " + myArrayList);
+
+
+        myArrayList.sort(Comparator.naturalOrder()); // Сортировка по возрастанию
+        System.out.println(myArrayList);
+
+        myArrayList.sort(Comparator.reverseOrder()); // Сортировка по убыванию
+        System.out.println(myArrayList);
+
 
         myArrayList.clear();
         System.out.println("After clearing: " + myArrayList);
--- src/MyArrayList.java
+++ src/MyArrayList.java
@@ -1,4 +1,6 @@
 import java.util.Arrays;
+import java.util.Comparator;
+
 public class MyArrayList<T> {
     private static final int DEFAULT_CAPACITY = 10;
     private Object[] elements;
@@ -63,5 +65,42 @@ public class MyArrayList<T> {
         }
         sb.append("]");
         return sb.toString();
+    }
+
+    public void sort(Comparator<? super T> comparator) {
+        quickSort(comparator, 0, size - 1);
+    }
+
+    private void quickSort(Comparator<? super T> comparator, int low, int high) {
+        if (low < high) {
+            int partitionIndex = partition(comparator, low, high);
+            quickSort(comparator, low, partitionIndex - 1);
+            quickSort(comparator, partitionIndex + 1, high);
+        }
+    }
+
+    private int partition(Comparator<? super T> comparator, int low, int high) {
+        T pivot = get(high);
+        int i = low - 1;
+        for (int j = low; j < high; j++) {
+            if (comparator.compare(get(j), pivot) <= 0) {
+                i++;
+                swap(i, j);
+            }
+        }
+        swap(i + 1, high);
+        return i + 1;
+    }
+    public void set(int index, T item) {
+        if (index < 0 || index >= size) {
+            throw new IndexOutOfBoundsException("Index out of bounds: " + index);
+        }
+        elements[index] = item;
+    }
+
+    private void swap(int i, int j) {
+        T temp = get(i);
+        set(i, get(j));
+        set(j, temp);
     }
 }
